3169ce3cd01ee1ea94517fab5ecf7918cd152a00
yoshoku <yoshoku@outlook.com>
2020-10-08 21:57:28 +0900

    :up: Bump up version number to 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 0.2.0
+- Add get_array and set_array methods to dump and load array data.
+
 ## 0.1.0
 - First release.
--- lib/dartsclone/version.rb
+++ lib/dartsclone/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module DartsClone
-  VERSION = '0.1.0'
+  VERSION = '0.2.0'
   DARTS_CLONE_VERSION = '0.32'
 end
